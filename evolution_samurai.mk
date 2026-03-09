346b909699485a41c38413f037a37f250f63e6a1
Mohammad Minhaz <usermohammad2003@gmail.com>
2026-03-10 01:10:23 +0600

    Update evolution_samurai.mk

--- evolution_samurai.mk
+++ evolution_samurai.mk
@@ -6,15 +6,15 @@
 
 $(call inherit-product, device/realme/samurai/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := samurai
 PRODUCT_MANUFACTURER := realme
 PRODUCT_MODEL := realme X2 Pro
-PRODUCT_NAME := lineage_samurai
+PRODUCT_NAME := evolution_samurai
 
 BUILD_FINGERPRINT := "realme/RMX1931/RMX1931L1:11/RKQ1.200928.002/1625457120608:user/release-keys"
 
@@ -24,3 +24,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1931"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Maintainer Info
+PRODUCT_MAINTAINER := Mohammad Minhaz
