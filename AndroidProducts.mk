32d3e4c87bce3945135f2238e607c5a17d73c85d
Mohammad Minhaz <usermohammad2003@gmail.com>
2026-03-12 11:31:40 -0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_samurai.mk
+    $(LOCAL_DIR)/pixelos_samurai.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_samurai-userdebug
+    pixelos_samurai-userdebug
